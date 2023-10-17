763564c3b5523b1e47529b85d5f46be26c7840cd
srgowen <srgowen2@gmail.com>
2023-10-16 22:29:21 -0400

    added comments and README

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,4 @@
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the HTML.
 $(function () { 
     // Get the current hour using the Day.js library
     let currentHour = dayjs().hour();
